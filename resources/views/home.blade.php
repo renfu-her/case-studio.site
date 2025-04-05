b3925b12a57f4794ee9a7c19725f1e60396e6a45
Patrick Her <renfu.her@gmail.com>
2025-04-05 21:45:56 +0800

    移除首頁中的顏色模式提示資訊，以簡化介面並提升整體視覺效果。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,28 +93,4 @@
     </div>
 </section>
 
-<!-- Color Mode Info -->
-<div class="mt-7 mt-sm-8">
-    <div class="max-w-4xl mx-auto" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
-        <div class="alert bg-info bg-opacity-25 d-flex p-3" role="alert">
-            <svg class="bi flex-shrink-0 me-2 text-info-emphasis" fill="currentColor" width="20" height="20" role="img" viewbox="0 0 16 16" aria-label="Info:">
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"></path>
-            </svg>
-            <div class="ps-1">
-                <h3 class="m-0 fw-semibold text-sm text-info-emphasis">
-                    Heads up! Color Mode Options:
-                </h3>
-                <div class="mt-2 text-sm">
-                    <p class="m-0 text-info-emphasis">
-                        To change the color mode of this template or adjust how they look in different color schemes, please locate the <span class="fst-italic fw-medium">Color Mode Button</span> at the top-right corner of the screen. The button offers three different color mode options: Light, Dark, and Auto.	
-                    </p>
-                    <hr class="border-info border-opacity-50">
-                    <p class="m-0 text-info-emphasis">
-                        Happy customizing! 🎨
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection 
