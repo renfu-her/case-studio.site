b5ba8e8d9f616875d6c4e8696e3e14d318379abe
Patrick Her <renfu.her@gmail.com>
2025-04-05 22:30:57 +0800

    新增服務頁面中的重定向功能，確保創建和編輯服務後能正確返回服務列表頁面，提升使用者體驗。

--- app/Filament/Resources/ServiceResource/Pages/EditService.php
+++ app/Filament/Resources/ServiceResource/Pages/EditService.php
@@ -16,4 +16,10 @@ class EditService extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
 }
