8b080362896493443afef5f40080de99b73150dc
Patrick Her <renfu.her@gmail.com>
2025-04-05 21:07:50 +0800

    在應用程式中新增 CSRF 令牌驗證例外路徑，並在佈局中加入 Bootstrap Icons 的樣式表，改善使用者介面與導航體驗。

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'backend/login',
+            'login'
+        ]);
+    })
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,7 @@
     @endif
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('assets/libraries/aos/aos.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
@@ -57,68 +58,76 @@
     <div class="py-4">
         <nav class="container">
             <div class="d-flex justify-content-between align-items-center">
-                <a href="{{ url('/') }}">
+                <a href="{{ url('/') }}" class="navbar-brand">
                     @if($logo = \App\Models\Setting::get('site_logo'))
                         <img src="{{ Storage::url($logo) }}" height="40" alt="{{ \App\Models\Setting::get('site_title', '網站標題') }}">
                     @endif
                 </a>
 
-                <div class="dropdown">
-                    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                        <symbol id="check2" viewbox="0 0 16 16">
-                            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"></path>
-                        </symbol>
-                        <symbol id="circle-half" viewbox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"></path>
-                        </symbol>
-                        <symbol id="moon-stars-fill" viewbox="0 0 16 16">
-                            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"></path>
-                            <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"></path>
-                        </symbol>
-                        <symbol id="sun-fill" viewbox="0 0 16 16">
-                            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
-                        </symbol>
-                    </svg>
-
-                    <button class="btn btn-primary text-white btn-sm rounded dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
-                        <svg fill="currentColor" class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-                        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-                    </button>
-
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-hover end-0 p-1 rounded-3 bg-body-tertiary shadow" style="--bs-dropdown-min-width: 9rem;" aria-labelledby="bd-theme-text">
-                        <li style="color: var(--bs-tertiary-bg);">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="d-inline-block position-absolute top-0 end-0 translate-middle mt-n1" viewbox="0 0 16 16">
-                                <path class="carret-dropdown-path" d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"></path>
-                            </svg>
+                <!-- Desktop Navigation Menu -->
+                <div class="d-none d-lg-block">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="{{ url('/') }}">
+                                首頁
+                            </a>
                         </li>
-
-                        <li>
-                            <button type="button" class="dropdown-item d-flex align-items-center rounded-1" data-bs-theme-value="light" aria-pressed="false">
-                                <svg fill="currentColor" class="bi me-2 theme-icon" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-                                Light
-                                <svg fill="currentColor" class="bi ms-auto d-none active-check" width="1em" height="1em"><use href="#check2"></use></svg>
-                            </button>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('projects*') ? 'active' : '' }}" href="{{ url('/projects') }}">
+                                案例
+                            </a>
                         </li>
-
-                        <li>
-                            <button type="button" class="dropdown-item d-flex align-items-center rounded-1 my-1" data-bs-theme-value="dark" aria-pressed="false">
-                                <svg fill="currentColor" class="bi me-2 theme-icon" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-                                Dark
-                                <svg fill="currentColor" class="bi ms-auto d-none active-check" width="1em" height="1em"><use href="#check2"></use></svg>
-                            </button>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('about*') ? 'active' : '' }}" href="{{ url('/about') }}">
+                                關於我們
+                            </a>
                         </li>
-
-                        <li>
-                            <button type="button" class="dropdown-item d-flex align-items-center rounded-1 active" data-bs-theme-value="auto" aria-pressed="true">
-                                <svg fill="currentColor" class="bi me-2 theme-icon" width="1em" height="1em"><use href="#circle-half"></use></svg>
-                                Auto
-                                <svg fill="currentColor" class="bi ms-auto d-none active-check" width="1em" height="1em"><use href="#check2"></use></svg>
-                            </button>
+                        <li class="nav-item">
+                            <a class="nav-link {{ request()->is('contact*') ? 'active' : '' }}" href="{{ url('/contact') }}">
+                                聯絡我們
+                            </a>
                         </li>
                     </ul>
                 </div>
+
+                <!-- Mobile Menu Button -->
+                <button class="btn btn-link d-lg-none p-0 text-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileMenu">
+                    <i class="bi bi-list fs-1"></i>
+                </button>
             </div>
         </nav>
+    </div>
+
+    <!-- Mobile Menu -->
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileMenu">
+        <div class="offcanvas-header border-bottom">
+            <h5 class="offcanvas-title fs-4">選單</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav">
+                <li class="nav-item mb-3">
+                    <a class="nav-link {{ request()->is('/') ? 'active' : '' }} fs-5" href="{{ url('/') }}">
+                        <i class="bi bi-house-door me-2"></i>首頁
+                    </a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link {{ request()->is('projects*') ? 'active' : '' }} fs-5" href="{{ url('/projects') }}">
+                        <i class="bi bi-grid me-2"></i>案例
+                    </a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link {{ request()->is('about*') ? 'active' : '' }} fs-5" href="{{ url('/about') }}">
+                        <i class="bi bi-info-circle me-2"></i>關於我們
+                    </a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link {{ request()->is('contact*') ? 'active' : '' }} fs-5" href="{{ url('/contact') }}">
+                        <i class="bi bi-envelope me-2"></i>聯絡我們
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 
     <!-- Main Content -->
