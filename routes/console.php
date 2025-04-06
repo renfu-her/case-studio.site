bf22314ae8fdf49c194bce126179663b20c02cc9
Patrick Her <renfu.her@gmail.com>
2025-04-06 20:01:28 +0800

    將郵件隊列處理命令從 Artisan 移至排程設定，並調整為每分鐘執行，以提升郵件處理的即時性與效率。

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Artisan::command('mail:process-queue', function () {
-    $this->comment('Processing mail queue...');
-})->purpose('Process the mail queue');
+
+Schedule::command('mail:process-queue')->everyMinute();
